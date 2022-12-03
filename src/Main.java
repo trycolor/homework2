b2c8a7cd7ec227178e4b27dca37028ed74c0d69a
Oleg <hillott@yandex.ru>
2022-12-04 02:14:14 +0300

    задача 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        var dog = 8;
+        var cat = 3.6;
+        var paper = 763789;
+        System.out.println(dog);
+        System.out.println(cat);
+        System.out.println(paper);
     }
 }
