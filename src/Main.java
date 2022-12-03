c34d98bd828028a67f7088259002b2db9e7d278c
Oleg <hillott@yandex.ru>
2022-12-04 02:32:33 +0300

    задача 3

--- src/Main.java
+++ src/Main.java
@@ -12,6 +12,12 @@ public class Main {
         System.out.println(dog);
         System.out.println(cat);
         System.out.println(paper);
+        var dog1 = dog-3.5;
+        var cat1 = cat-1.6;
+        var paper1 = paper-7639;
+        System.out.println(dog1);
+        System.out.println(cat1);
+        System.out.println(paper1);
 
     }
 }
